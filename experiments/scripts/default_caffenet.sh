9bfc6b070c00e4730cc3123cd0524aac2dbaa0ba
Anting Shen <shenantingtt@hotmail.com>
2015-05-07 10:47:57 -0700

    change models and experiment scripts to run no_bbox on voc logo data

--- experiments/scripts/default_caffenet.sh
+++ experiments/scripts/default_caffenet.sh
@@ -9,12 +9,12 @@ LOG="experiments/logs/default_caffenet.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
 exec &> >(tee -a "$LOG")
 echo Logging output to "$LOG"
 
-time ./tools/train_net.py --gpu 1 \
+time ./tools/train_net.py --gpu 0 \
   --solver models/CaffeNet/solver.prototxt \
   --weights data/imagenet_models/CaffeNet.v2.caffemodel \
   --imdb voc_logo_trainval
 
-time ./tools/test_net.py --gpu 1 \
+time ./tools/test_net.py --gpu 0 \
   --def models/CaffeNet/test.prototxt \
   --net output/default/voc_logo_trainval/caffenet_fast_rcnn_iter_40000.caffemodel \
   --imdb voc_logo_test
--- experiments/scripts/no_bbox_reg_vgg16.sh
+++ experiments/scripts/no_bbox_reg_vgg16.sh
@@ -9,14 +9,14 @@ LOG="experiments/logs/no_bbox_reg_vgg16.txt.`date +'%Y-%m-%d_%H-%M-%S'`"
 exec &> >(tee -a "$LOG")
 echo Logging output to "$LOG"
 
-time ./tools/train_net.py --gpu $1 \
+time ./tools/train_net.py --gpu 0 \
   --solver models/VGG16/no_bbox_reg/solver.prototxt \
   --weights data/imagenet_models/VGG16.v2.caffemodel \
-  --imdb voc_2007_trainval \
+  --imdb voc_logo_trainval \
   --cfg experiments/cfgs/no_bbox_reg.yml
 
-time ./tools/test_net.py --gpu $1 \
+time ./tools/test_net.py --gpu 0 \
   --def models/VGG16/no_bbox_reg/test.prototxt \
-  --net output/no_bbox_reg/voc_2007_trainval/vgg16_fast_rcnn_no_bbox_reg_iter_40000.caffemodel \
-  --imdb voc_2007_test \
+  --net output/no_bbox_reg/voc_logo_trainval/vgg16_fast_rcnn_no_bbox_reg_iter_40000.caffemodel \
+  --imdb voc_logo_test \
   --cfg experiments/cfgs/no_bbox_reg.yml
