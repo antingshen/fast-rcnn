42c77ce6a1f83780ee3778a7239c91d58406e8ab
Anting Shen <shenantingtt@hotmail.com>
2015-05-07 23:21:54 -0700

    first try at classifier pr curves

--- lib/datasets/VOCdevkit-matlab-wrapper/voc_eval.m
+++ lib/datasets/VOCdevkit-matlab-wrapper/voc_eval.m
@@ -11,9 +11,49 @@ end
 fprintf('\n~~~~~~~~~~~~~~~~~~~~\n');
 fprintf('Results:\n');
 aps = [res(:).ap]';
+% TODO: remove HP
 fprintf('%.1f\n', aps * 100);
-fprintf('%.1f\n', mean(aps) * 100);
+fprintf('Mean mAP: %.1f\n', mean(aps) * 100);
 fprintf('~~~~~~~~~~~~~~~~~~~~\n');
+
+all_tp = res(1).tp;
+all_fp = res(1).fp;
+all_npos = res(1).npos;
+
+for i = 2:length(VOCopts.classes)
+  all_tp = all_tp + res(i).tp;
+  all_fp = all_fp + res(i).fp;
+  all_npos = all_npos + res(i).npos;
+end
+
+all_rec = all_tp / all_npos;
+all_prec = all_tp ./ (all_tp + all_fp);
+
+all_ap=0;
+for t=0:0.1:1
+    p=max(all_prec(all_rec>=t));
+    if isempty(p)
+        p=0;
+    end
+    all_ap=all_ap+p;
+end
+all_ap = all_ap/11;
+
+% plot overall precision/recall
+plot(all_rec,all_prec,'-');
+grid;
+xlabel 'recall'
+ylabel 'precision'
+title(sprintf('overall: %s, subset: %s, AP = %.3f',cls,VOCopts.testset,all_ap));
+
+all_ap_auc = xVOCap(all_rec, all_prec);
+
+% force plot limits
+ylim([0 1]);
+xlim([0 1]);
+
+print(gcf, '-djpeg', '-r0', ...
+[output_dir '/overall_pr.jpg']);
 
 function res = voc_eval_cls(cls, VOCopts, comp_id, output_dir, rm_res)
 
@@ -33,7 +73,7 @@ do_eval = 1; %(str2num(year) <= 2007) | ~strcmp(test_set, 'test');
 if do_eval
   % Bug in VOCevaldet requires that tic has been called first
   tic;
-  [recall, prec, ap] = VOCevaldet(VOCopts, comp_id, cls, true);
+  [recall, prec, ap, tp, fp, npos] = VOCevaldet(VOCopts, comp_id, cls, true);
   ap_auc = xVOCap(recall, prec);
 
   % force plot limits
@@ -49,9 +89,12 @@ res.recall = recall;
 res.prec = prec;
 res.ap = ap;
 res.ap_auc = ap_auc;
+res.tp = tp;
+res.fp = fp;
+res.npos = npos;
 
 save([output_dir '/' cls '_pr.mat'], ...
-     'res', 'recall', 'prec', 'ap', 'ap_auc');
+     'res', 'recall', 'prec', 'ap', 'ap_auc', 'tp', 'fp', 'npos');
 
 if rm_res
   delete(res_fn);
